a80419a0161e0b8bf4db3867331116058f1000ab
milindkarpe <milind.karpe@gmail.com>
2012-09-09 17:29:15 +0530

    CUSTOMER WITH SP

---
+++ SagProject/SAG/Admin/default.aspx
@@ -0,0 +1,28 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Admin/admin.Master" AutoEventWireup="true"
+    CodeBehind="default.aspx.cs" Inherits="SAG._default" %>
+
+<asp:Content ID="Content1" ContentPlaceHolderID="head" runat="server">
+</asp:Content>
+<asp:Content ID="Content2" ContentPlaceHolderID="ContentPlaceHolder1" runat="server">
+    <header class="jumbotron subhead" id="overview">
+        <div class="page-header">
+            <h1>
+                Typography <small>Headings, paragraphs, lists, and other inline type elements</small></h1>
+        </div>
+        <div>
+            
+            <asp:GridView ID="Repeater1" runat="server" BackColor="White" 
+                BorderColor="#CCCCCC" BorderStyle="None" BorderWidth="1px" CellPadding="3">
+                <FooterStyle BackColor="White" ForeColor="#000066" />
+                <HeaderStyle BackColor="#006699" Font-Bold="True" ForeColor="White" />
+                <PagerStyle BackColor="White" ForeColor="#000066" HorizontalAlign="Left" />
+                <RowStyle ForeColor="#000066" />
+                <SelectedRowStyle BackColor="#669999" Font-Bold="True" ForeColor="White" />
+                <SortedAscendingCellStyle BackColor="#F1F1F1" />
+                <SortedAscendingHeaderStyle BackColor="#007DBB" />
+                <SortedDescendingCellStyle BackColor="#CAC9C9" />
+                <SortedDescendingHeaderStyle BackColor="#00547E" />
+            </asp:GridView>
+        </div>
+    </header>
+</asp:Content>
--- SagProject/SAGDAL/CustomerDAL.cs
+++ SagProject/SAGDAL/CustomerDAL.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Data;
+using System.Data.SqlClient;
 using System.Text;
 using SAGDAL;
 
@@ -8,11 +9,68 @@ namespace SAGDAL
 {
     public class CustomerDAL : BaseClassDAL
     {
-        public DataTable BindPosts()
+
+        public string SaveCustomerDAL(string code,string introducer,string preName,string name,string preSdw, string sdwName,string pancard,string email,string preAdd,string perAdd,string prePin,string perPin,string mobile,
+            string nominee,string telephone,string relation,DateTime dob,DateTime nomDob,int age,int nomAge,DateTime StartDate,int BranchId,string preNom)
         {
-            return DAL.ExecuteReader("select * from branch");
+            SqlParameter[] ParameterArray = {
+                                                new SqlParameter("@introducer", SqlDbType.VarChar),
+                                                new SqlParameter("@preName", SqlDbType.VarChar),
+                                                new SqlParameter("@Name", SqlDbType.VarChar),
+                                                new SqlParameter("@preSdw", SqlDbType.VarChar),
+                                                new SqlParameter("@sdwName", SqlDbType.VarChar),
+                                                new SqlParameter("@pancard", SqlDbType.VarChar),
+                                                new SqlParameter("@email", SqlDbType.VarChar),
+                                                new SqlParameter("@preAddress", SqlDbType.Text),
+                                                new SqlParameter("@perAddress", SqlDbType.Text),
+                                                new SqlParameter("@prePincode", SqlDbType.VarChar),
+                                                new SqlParameter("@perPincode", SqlDbType.VarChar),
+                                                new SqlParameter("@mobile", SqlDbType.VarChar),
+                                                new SqlParameter("@nominee", SqlDbType.VarChar),
+                                                new SqlParameter("@telephone", SqlDbType.VarChar),
+                                                new SqlParameter("@relation", SqlDbType.VarChar),
+                                                new SqlParameter("@dob ", SqlDbType.Date),
+                                                new SqlParameter("@nomDob ", SqlDbType.Date),
+                                                new SqlParameter("@age ", SqlDbType.Int),
+                                                new SqlParameter("@nomAge ", SqlDbType.Int),
+                                                new SqlParameter("@startDate ", SqlDbType.Date),
+                                                new SqlParameter("@branchId", SqlDbType.VarChar),
+                                                new SqlParameter("@preNom", SqlDbType.VarChar),
+                                                new SqlParameter("@code", SqlDbType.VarChar)
+                                              };
+
+                ParameterArray[0].Value = introducer;
+                ParameterArray[1].Value = preName;
+                ParameterArray[2].Value = name;
+                ParameterArray[3].Value = preSdw;
+                ParameterArray[4].Value = sdwName;
+                ParameterArray[5].Value = pancard;
+                ParameterArray[6].Value = email;
+                ParameterArray[7].Value = preAdd;
+                ParameterArray[8].Value = perAdd;
+                ParameterArray[9].Value = prePin;
+                ParameterArray[10].Value = perPin;
+                ParameterArray[11].Value = mobile;
+                ParameterArray[12].Value = nominee;
+                ParameterArray[13].Value = telephone;
+                ParameterArray[14].Value = relation;
+                ParameterArray[15].Value = dob;
+                ParameterArray[16].Value = nomDob;
+                ParameterArray[17].Value = age;
+                ParameterArray[18].Value = nomAge;
+                ParameterArray[19].Value = StartDate;
+                ParameterArray[20].Value = BranchId;
+                ParameterArray[21].Value = preNom;
+                ParameterArray[22].Value = code;
+
+                return DAL.ExecuteMMScalarSP("SaveCustomer", ParameterArray);
+
         }
 
-       
+        public DataSet BindPosts()
+        {
+            return DAL.ExecuteDatasetSP("spBindPosts");
+        }
+
     }
 }
